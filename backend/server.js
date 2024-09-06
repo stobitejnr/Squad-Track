e2f5947153213ca543b0df81cbc70533dbfed2a3
Ben <sambe016@student.otago.ac.nz>
2024-09-06 23:19:52 +1200

    reset vm connections

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 5002;
+const port = 3000;
 
-// Middleware
-app.use(express.json());
-
-// Database setup
-const db = require('./database');
-
-// Routes
-app.get('/api/data', (req, res) => {
-    db.query('SELECT * FROM players', (err, results) => {
-        if (err) throw err;
-        res.json(results);
-    });
+app.get('/', (req, res) => {
+    res.send('Backend connected to the database!');
 });
 
 app.listen(port, () => {
-    console.log(`Backend running on port ${port}`);
+    console.log(`Backend listening at http://localhost:${port}`);
 });
