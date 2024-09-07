6e1471f877c67fa952ba061444d83ac68f1b3d90
Ben <sambe016@student.otago.ac.nz>
2024-09-07 15:27:28 +1200

    updating ui

--- database/init.sql
+++ database/init.sql
@@ -1,26 +1,26 @@
 CREATE DATABASE IF NOT EXISTS simpledb;
 USE simpledb;
 
-drop table if exists players;
+DROP TABLE IF EXISTS players;
 
 CREATE TABLE players
 (
     id         INT AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(100) NOT NULL,
-    email      VARCHAR(100) NOT NULL UNIQUE,
-    number     VARCHAR(15),
-    position   VARCHAR(50),
+    name       VARCHAR(100)   NOT NULL,
+    age        VARCHAR(10)    NOT NULL,
+    salary     DECIMAL(10, 2) NOT NULL,
+    position   VARCHAR(50)    NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO players (name, email, number, position)
-VALUES ('John Doe', 'john.doe@example.com', '1234567890', 'Manager');
+INSERT INTO players (name, age, salary, position)
+VALUES ('John Brown', '48', 140.00, 'Center Midfielder');
 
-INSERT INTO players (name, email, number, position)
-VALUES ('Jane Smith', 'jane.smith@example.com', '0987654321', 'Developer');
+INSERT INTO players (name, age, salary, position)
+VALUES ('Jane Smith', '25', 120.00, 'Right Winger');
 
-INSERT INTO players (name, email, number, position)
-VALUES ('Emily Johnson', 'emily.johnson@example.com', '5551234567', 'Designer');
+INSERT INTO players (name, age, salary, position)
+VALUES ('Emily Johnson', '35', 150.00, 'Goalkeeper');
 
-INSERT INTO players (name, email, number, position)
-VALUES ('Michael Brown', 'michael.brown@example.com', '4449876543', 'Sales Representative');
+INSERT INTO players (name, age, salary, position)
+VALUES ('Michael Brown', '20', 130.00, 'Left Back');
--- frontend/script.js
+++ frontend/script.js
@@ -2,15 +2,27 @@ async function fetchData() {
     try {
         const response = await fetch('http://localhost:3000/data');
         const data = await response.json();
-        const tableBody = document.querySelector('#main-table tbody');
+        const tableBody = document.querySelector('#main-table');
+
         data.forEach(row => {
+            console.log(row);
             const tr = document.createElement('tr');
             tr.innerHTML = `
-                        <td>${row.name}</td>
-                        <td>${row.email}</td>
-                        <td>${row.number}</td>
-                        <td>${row.position}</td>
-                    `;
+                <td class="py-3 ps-4">
+                    <div class="flex items-center h-5">
+                        <input class="border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" type="checkbox">
+                    </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">${row.name}</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">${row.age} years</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">$${parseFloat(row.salary).toFixed(2)}</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">${row.position}</td>
+                <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                    <button class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400" type="button">
+                        Delete
+                    </button>
+                </td>
+            `;
             tableBody.appendChild(tr);
         });
     } catch (error) {
@@ -19,23 +31,3 @@ async function fetchData() {
 }
 
 fetchData();
-
-document.getElementById('new-button').addEventListener('click', function () {
-    // Get the table element
-    let table = document.getElementById('main-table').getElementsByTagName('tbody')[0];
-
-    // Create a new row
-    let newRow = table.insertRow();
-
-    // Add cells to the row and fill them with content
-    let cell1 = newRow.insertCell(0);
-    let cell2 = newRow.insertCell(1);
-    let cell3 = newRow.insertCell(2);
-    let cell4 = newRow.insertCell(3);
-
-    // Add content to the cells
-    cell1.textContent = 'New Row Col 1';
-    cell2.textContent = 'New Row Col 2';
-    cell3.textContent = 'New Row Col 3';
-    cell4.textContent = 'New Row Col 4';
-});
